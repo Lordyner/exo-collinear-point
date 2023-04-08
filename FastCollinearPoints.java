4202ae435af1a89abe901c9a4cd21f97a16cea3b
Thomas André-Lubin <thomas.andrelubin@gmail.com>
2023-04-08 10:07:59 +0200

    Update main method to test through file

--- FastCollinearPoints.java
+++ FastCollinearPoints.java
@@ -92,23 +92,23 @@ public class FastCollinearPoints {
     public static void main(String[] args) {
 
         // read the n points from a file
-        //In in = new In(args[0]);
-//        int n = in.readInt();
-        Point[] points = new Point[8];
-//        for (int i = 0; i < n; i++) {
-//            int x = in.readInt();
-//            int y = in.readInt();
-//            points[i] = new Point(x, y);
-//        }
-        
-        points[0] = new Point(10000, 0);
-        points[1] = new Point(0, 10000);
-        points[2] = new Point(3000, 7000);
-        points[3] = new Point(7000, 3000);
-        points[4] = new Point(20000, 21000);
-        points[5] = new Point(3000, 4000);
-        points[6] = new Point(14000, 15000);
-        points[7] = new Point(6000, 7000);
+        In in = new In(args[0]);
+        int n = in.readInt();
+        Point[] points = new Point[n];
+        for (int i = 0; i < n; i++) {
+            int x = in.readInt();
+            int y = in.readInt();
+            points[i] = new Point(x, y);
+        }
+        // Points du fichier input8.txt pour débug
+//        points[0] = new Point(10000, 0);
+//        points[1] = new Point(0, 10000);
+//        points[2] = new Point(3000, 7000);
+//        points[3] = new Point(7000, 3000);
+//        points[4] = new Point(20000, 21000);
+//        points[5] = new Point(3000, 4000);
+//        points[6] = new Point(14000, 15000);
+//        points[7] = new Point(6000, 7000);
         // draw the points
         StdDraw.enableDoubleBuffering();
         StdDraw.setXscale(0, 32768);
